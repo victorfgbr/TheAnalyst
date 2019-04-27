67352df3176e471e055f80efcbbe0577ce8255b3
Victor Goulart <victorfgbr@gmail.com>
2019-04-27 12:10:48 -0300

    Versão 1.00: Incluindo o menu dividindo a tela

--- index.php
+++ index.php
@@ -1,5 +1,52 @@
-<html>
+<html> 
+
   <head>
+    <title>The Analyst</title>
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link rel="stylesheet" type="text/css" href="css/menu.css">
   </head>
   
+  <body>
+  
+    <section class="menu">
+      <div class="menu-container">
+        <a class="menu-logo" href="#" > <img src="img\logo.png"> </a>
+        
+        <div class="menu-nav">
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Top News</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </section>
+    
+    <div class="main">
+      <section class="sec-1">
+        <div class="sec-container">
+          <div class="sec-box b-8">1.1</div>
+          <div class="sec-box b-4">1.2</div>
+        </div> 
+      </section>
+    
+      <section class="sec-2">
+        <div class="sec-container">
+          <div class="sec-box b-3">2.1</div>
+          <div class="sec-box b-3">2.2</div>
+          <div class="sec-box b-3">2.3</div>
+          <div class="sec-box b-3">2.4</div>
+        </div>
+      </section>
+      
+      <section class="sec-3">
+        <div class="sec-container">
+          <div class="sec-box b-4">3.1</div>
+          <div class="sec-box b-4">3.2</div>
+          <div class="sec-box b-4">3.3</div>
+        </div>
+      </section>    
+    </div>
+  </body>
+
 </html>
